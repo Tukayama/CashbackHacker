f31f35636cce24765bfc9a2e9fb1bfa1c46647c7
Tukayama <ya.tumanov2009@yandex.ru>
2021-11-18 18:23:01 +0300

    init commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,33 +3,37 @@ package ru.netology.service;
 //import org.junit.jupiter.api.Test;
 
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 //import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-   @Test
-     public void missingAmount() {
+    @Test
+    public void missingAmount() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
-        int expected =100;
+        int expected = 100;
         int astual = cashbackHackService.remain(amount);
         assertEquals(expected, astual);
     }
+
     @Test
-    public void boundaryValuesOfZero(){
-        CashbackHackService cashbackHackService=new CashbackHackService();
-        int amount=0;
-        int expected =1000;
+    public void boundaryValuesOfZero() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
         int astual = cashbackHackService.remain(amount);
-        assertEquals(expected,astual);
-    }@Test
-    public void maximumBoundaryValues(){
-        CashbackHackService cashbackHackService=new CashbackHackService();
-        int amount=1000;
-        int expected =1000;
+        assertEquals(expected, astual);
+    }
+
+    @Test
+    public void maximumBoundaryValues() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 1000;
         int astual = cashbackHackService.remain(amount);
-        assertEquals(expected,astual);
+        assertEquals(expected, astual);
     }
 
 }
